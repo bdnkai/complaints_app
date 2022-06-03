15ecac6b1bd4c559702de0c43a6522c1aa95b5fd
bdnkai <bdn.kai@gmail.com>
2022-06-02 20:05:42 -0700

    include a working function to pull the api with correct data

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const BASE_URL = 'https://data.cityofnewyororksk.us/resource/erm2-nwe9.json?agen
 /*----- app's state (variables) -----*/
 let numRequest = '';
 let new_Url = '';
+
 /*----- cached element references -----*/
 let inputRequest_El = document.querySelector('.request-amount')
 let requestBrook_El = document.querySelector('.requestBrook');
@@ -54,23 +55,32 @@ function newButtons(){
 function brooklynResults(){
     newURL("BROOKLYN")
     // event.preventDefault();
+    results_El.innerHTML = '';
+
     fetch(new_Url)
         .then(function (res){
             console.log(res)
             return res.json()
         })
         .then(function(data){
-            console.log(data);
-            console.log(`complaint: ${data[0].complaint_type} What the police did: ${data[0].resolution_description}`);
+            for (let i=0; i < data.length; i++){
+                let listItem = document.createElement('li')
+                let listHTML = `${data[i].complaint_type} ${newButtons()}<span class="resolution hidden">${data[i].resolution_description}</span>`
+                listItem.innerHTML = listHTML;
+                results_El.appendChild(listItem);
+            }
+            
     })
         
-    for (let i = 0; i < numRequest; i++){
-        newButtons()
-    }
+    
     
 }
     
-
+let newPull = (data) => {
+    for (let i = 0; i < numRequest; i++){
+        newButtons()
+    }
+}
 
 function manResults(){
     console.log('Manhatten works')
